4b4bc0eb4abb887e4e76931da77440975db0f0fe
AtharvVerma-290 <atharv.verma29k@gmail.com>
2025-12-21 20:15:03 +0530

    postgres connection inside extract task

--- dags/forex_etl.py
+++ dags/forex_etl.py
@@ -4,7 +4,10 @@ from data_ingestion import TwelveDataIngestor #type: ignore
 from supabase import create_client, Client
 from dotenv import load_dotenv
 import os
+import psycopg2
 load_dotenv()
+
+
 
 default_args = {
     'owner': 'atharv',
@@ -17,25 +20,55 @@ def forex_etl_pipeline():
 
     @task
     def extract_data():
-        url = os.environ.get("SUPABASE_URL")
-        key = os.environ.get("SUPABASE_KEY")
-        supabase: Client = create_client(url, key)
-
         data_ingestor = TwelveDataIngestor()
         extracted_data = data_ingestor.ingest(symbol="EUR/USD")
 
         print("Adding extracted_data to database")
         record = extracted_data[0]
-        extracted_data = supabase.table("EUR_USD_raw").upsert(
-            {
-                "datetime": record["datetime"],
-                "open": float(record["open"]),
-                "high": float(record["high"]),
-                "low": float(record["low"]),
-                "close": float(record["close"]),
-            },
-            on_conflict="datetime"
-        ).execute()
+        
+        try: 
+            conn = psycopg2.connect(database="app_db", user="admin", password="admin", host="app-postgres", port="5432")
+            print("Database connected successfully.")
+            
+            cur = conn.cursor()
+            cur.execute("""
+            CREATE TABLE IF NOT EXISTS eur_usd_raw(
+                id SERIAL PRIMARY KEY,
+                datetime DATE NOT NULL UNIQUE,
+                open NUMERIC,
+                high NUMERIC,
+                low NUMERIC,
+                close NUMERIC
+            );
+            """)            
+            print("Table created successfully.")
+
+            query = """
+            INSERT INTO eur_usd_raw (datetime, open, high, low, close) VALUES
+            (%s, %s, %s, %s, %s)
+            ON CONFLICT (datetime) DO UPDATE SET
+                open = EXCLUDED.open,
+                high = EXCLUDED.high,
+                low = EXCLUDED.low,
+                close = EXCLUDED.close;    
+            """
+
+            values = (
+                record["datetime"],
+                record["open"],
+                record["high"],
+                record["low"],
+                record["close"]
+            )
+
+            cur.execute(query, values)
+            print("Record added to the database.")
+            conn.commit()
+            conn.close()
+        
+        except Exception as e:
+            print(e)
+        
         print(record)
         return "EUR_USD_raw"
 
@@ -55,6 +88,6 @@ def forex_etl_pipeline():
         pass
     
     raw_table = extract_data()
-    transform_data(raw_table=raw_table)
+    # transform_data(raw_table=raw_table)
 
 etl_pipeline = forex_etl_pipeline()
