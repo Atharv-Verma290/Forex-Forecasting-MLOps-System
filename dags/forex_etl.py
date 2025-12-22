9edca8e7cc610d47b9bc7e7920f793e24d155822
AtharvVerma-290 <atharv.verma29k@gmail.com>
2025-12-22 19:36:51 +0530

    made basic load task

--- dags/forex_etl.py
+++ dags/forex_etl.py
@@ -71,42 +71,65 @@ def forex_etl_pipeline():
     def transform_data(raw_table):
         conn = psycopg2.connect(database="app_db", user="admin", password="admin", host="app-postgres", port="5432")
         print("Database connected successfully.")
-        cur = conn.cursor()
+        # cur = conn.cursor()
 
-        query=f"""
-        SELECT * FROM {raw_table};
-        """
-        cur.execute(query)
-        raw_data = cur.fetchall()
+        # query=f"""
+        # SELECT * FROM {raw_table};
+        # """
+        # cur.execute(query)
+        # raw_data = cur.fetchall()
+        # raw_data = pd.DataFrame(raw_data, columns=['id', 'datetime', 'open', 'high', 'low', 'close'])
 
-        raw_data = pd.DataFrame(raw_data, columns=['id', 'datetime', 'open', 'high', 'low', 'close'])
+        raw_data = pd.read_sql(
+            f"SELECT * FROM {raw_table} ORDER BY datetime DESC;",
+            conn
+        )
         
         transformer = ForexDataTransformation(raw_data)
         transformed_data = transformer.apply_transformation()
         
-        cur.execute("""
-            CREATE TABLE IF NOT EXISTS eur_usd_features(
-            id SERIAL PRIMARY KEY,
-            datetime DATE NOT NULL UNIQUE,
-            open NUMERIC,
-            high NUMERIC,
-            low NUMERIC,
-            close NUMERIC,
-            close_ratio_2 NUMERIC,
-            close_ratio_5 NUMERIC,
-            close_ratio_60 NUMERIC,
-            close_ratio_250 NUMERIC,
-            close_ratio_1000 NUMERIC
+        cur = conn.cursor()
+        staging_table = "eur_usd_staging"
+
+        cur.execute(f"DROP TABLE IF EXISTS {staging_table};")
+
+        columns_sql = []
+        for col, dtype in transformed_data.dtypes.items():
+            if col == "id":
+                sql_type = "SERIAL PRIMARY KEY"
+            elif col == "datetime":
+                sql_type = "DATE NOT NULL UNIQUE"
+            elif "float" in str(dtype):
+                sql_type = "NUMERIC"
+            elif "int" in str(dtype):
+                sql_type = "INTEGER"
+            else:
+                sql_type = "TEXT"
+            columns_sql.append(f"{col} {sql_type}")
+        
+        create_sql = f"""
+            CREATE TABLE {staging_table} (
+                {', '.join(columns_sql)}
             );
-        """)
+        """
+        cur.execute(create_sql)
         conn.commit()
+
+        # buffer = StringIO()
+        # df_features.to_csv(buffer, index=False, header=False)
+        # buffer.seek(0)
+
+        # cur.copy_expert(
+        #     f"COPY {staging_table} FROM STDIN WITH CSV",
+        #     buffer
+        # )
+        # conn.commit()
 
         tuples = [tuple(x) for x in transformed_data.to_numpy()]
         cols = ', '.join(list(transformed_data.columns))
-
-        query = "INSERT INTO eur_usd_features (%s) VALUES %%s" % (cols)
+        insertion_query = "INSERT INTO %s (%s) VALUES %%s" % (staging_table, cols)
         try:
-            extras.execute_values(cur, query, tuples)
+            extras.execute_values(cur, insertion_query, tuples)
             conn.commit()
         except (Exception, psycopg2.DatabaseError) as error:
             print("Error: %s" % error)
@@ -114,16 +137,33 @@ def forex_etl_pipeline():
             cur.close()
             return 1
         
-        print("The transformed_data inserted in db")
+        print("transformed data into staging.")
+        conn.commit()
         cur.close()
         conn.close()
 
-        
+        return staging_table
+
     @task 
-    def load_data():
-        pass
+    def load_data(staging_table):
+        conn = psycopg2.connect(database="app_db", user="admin", password="admin", host="app-postgres", port="5432")
+        final_table = "eur_usd_final"
+        swap_query = f"""
+            BEGIN;
+            DROP TABLE IF EXISTS {final_table};
+            ALTER TABLE {staging_table} RENAME TO {final_table};
+            COMMIT;
+        """
+        cur = conn.cursor()
+        cur.execute(swap_query)
+        conn.commit()
+        cur.close()
+        conn.close()
+        print("Final data added.")
+
     
     raw_table = extract_data()
-    transform_data(raw_table=raw_table)
+    staging_table = transform_data(raw_table=raw_table)
+    load_data(staging_table)
 
 etl_pipeline = forex_etl_pipeline()
